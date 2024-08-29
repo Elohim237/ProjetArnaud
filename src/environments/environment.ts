203cb7625f58550a971f04e9ec09d0c11cd3676c
JuniorMSL <junior@msl-itech.com>
2024-08-29 09:55:14 +0100

    second commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+import { LOGIN_TYPE } from 'src/app/constant';
+
+export const environment = {
+  production: false,
+  title: 'S3 Manager',
+  logins: [
+    {
+      type: LOGIN_TYPE.cognito,
+      name: 'Amazon Cognito',
+      region: '',
+      identityPoolId: '',
+      userPoolId: '',
+      userPoolWebClientId: '',
+    },
+    {
+      type: LOGIN_TYPE.accessKey,
+      name: 'AWS Access Key',
+      region: 'us-east-1',
+    },
+  ],
+  accessKey: '',
+  secretKey: '',
+  username: '',
+  password: '',
+  bucketNames: [
+    'recording-huios',
+  ]
+};
